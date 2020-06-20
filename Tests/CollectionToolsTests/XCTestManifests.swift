4d9c7837cc48e3ecea1850e7cb2ea0fbc385e906
Ben Leggiero <BenLeggiero@Gmail.com>
2020-06-20 15:32:31 -0600

    Tests for CollectionWhichCanBeEmpty

--- Tests/CollectionToolsTests/XCTestManifests.swift
+++ Tests/CollectionToolsTests/XCTestManifests.swift
@@ -4,6 +4,7 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(Collection_plus_everyPair_Tests.allTests),
+        testCase(CollectionWhichCanBeEmpty_Tests.allTests),
     ]
 }
 #endif
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,5 +2,6 @@ import XCTest
 import CollectionToolsTests
 
 XCTMain([
-    Collection_plus_everyPair_Tests.allTests
+    Collection_plus_everyPair_Tests.allTests,
+    CollectionWhichCanBeEmpty_Tests.allTests,
 ])
