e86203f01268d9a63f5b095822abee984e6f5709
Ky Leggiero <KyLeggiero@Gmail.com>
2022-11-25 02:12:52 -0700

    Fixed documentation

--- Sources/CollectionTools/RangeReplaceableCollection + elemental operators.swift
+++ Sources/CollectionTools/RangeReplaceableCollection + elemental operators.swift
@@ -12,9 +12,9 @@ import Foundation
 
 public extension RangeReplaceableCollection {
     
-    /// Creates a new collection by concatenating the given elements onto the end of the collections.
+    /// Creates a new collection by concatenating the given element onto the end of the collection.
     ///
-    /// The first argument's `Element` type must be the same type as the second element. For example, you can
+    /// The first argument's `Element` subtype must be the same type as the second element. For example, you can
     /// concatenate an integer onto an integer array.
     ///
     /// ```swift
@@ -25,7 +25,7 @@ public extension RangeReplaceableCollection {
     /// ```
     ///
     /// The resulting collection has the type of the argument on the left-hand side. In the example above,
-    /// fibonacciUpTo8 has the same type as fibonacciUpTo5, which is `[Int]`.
+    /// `fibonacciUpTo8` has the same type as `fibonacciUpTo5`, which is `[Int]`.
     ///
     /// - Parameters:
     ///   - collection: A range-replaceable collection.
@@ -39,7 +39,7 @@ public extension RangeReplaceableCollection {
     /// Appends the given element to a range-replaceable collection.
     ///
     /// Use this operator to append an element to the end of a range-replaceable collection whose elements are that
-    /// same `Element` type. This example appends the an `Int` to an array of integers.
+    /// same `Element` type. This example appends an `Int` to an array of integers:
     ///
     /// ```swift
     /// var fibonacci = [0, 1, 1, 2, 3, 5]
